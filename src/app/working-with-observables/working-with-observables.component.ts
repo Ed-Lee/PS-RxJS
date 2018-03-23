f847c4868aea39623455d1a050d8e82ce77fe1f9
Ed Lee <ronder.lee@gmail.com>
2018-03-24 00:42:20 +0800

    Processing Mouse Events 利用Observable.fromEvent(document, 'mousemove')將mouse move event轉換成observable stream, 配合map, filter, delay 等operators

--- src/app/working-with-observables/working-with-observables.component.ts
+++ src/app/working-with-observables/working-with-observables.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/fromEvent";
+import {delay, filter, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-working-with-observables',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkingWithObservablesComponent implements OnInit {
 
+  mouseXY = {
+    x: 0,
+    y: 0
+  };
+
   constructor() { }
 
   ngOnInit() {
+    let source$ = Observable.fromEvent(document, 'mousemove')
+      .pipe(
+        map(
+          (m: MouseEvent) => {
+            return {
+              x: m.clientX,
+              y: m.clientY
+            }
+          }
+        ),
+        filter(value => value.x < 500),
+        delay(300)
+      );
+
+    source$.subscribe(
+      value => {
+        this.mouseXY = value;
+        console.log(value)
+      }
+    )
   }
 
 }
