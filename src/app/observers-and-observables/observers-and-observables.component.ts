ce3e4788e5935636f275883f5bebc1914b05fa02
Ed Lee <ronder.lee@gmail.com>
2018-03-23 15:10:16 +0800

    建立第一個Observable and Observer v1.

--- src/app/observers-and-observables/observers-and-observables.component.ts
+++ src/app/observers-and-observables/observers-and-observables.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/from";
 
 @Component({
   selector: 'app-observers-and-observables',
@@ -10,6 +12,23 @@ export class ObserversAndObservablesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let numbers = [1, 5, 9];
+    let source = Observable.from(numbers);
+
+    let myObserver = {
+      next(value) {
+        console.log(`value: ${value}`);
+      },
+      error(e) {
+        console.log(`error: ${e}`);
+      },
+      complete() {
+        console.log('complete');
+      }
+    }
+
+    source.subscribe(myObserver);
+    source.subscribe(myObserver);
   }
 
 }
