786d4c860b679a3168612eda7e4e75186d7a07fa
Ed Lee <ronder.lee@gmail.com>
2018-03-26 21:11:48 +0800

    Sending Request with XmlHttpRequest 用Observable.create(observer => {})將XmlHttpRequest取得的資料變成Observable
    Using flatMap Process Inner Observable
    當ㄧ個Observable.subscribe(裡面又有一層Observable.subscribe)，此時可使用flatMap將內層的subscribe結果往外層搬)

--- src/app/working-with-observables/working-with-observables.component.ts
+++ src/app/working-with-observables/working-with-observables.component.ts
@@ -1,43 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/observable/fromEvent";
-import {delay, filter, map} from "rxjs/operators";
+import {flatMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-working-with-observables',
   templateUrl: './working-with-observables.component.html',
   styleUrls: ['./working-with-observables.component.css']
 })
-export class WorkingWithObservablesComponent implements OnInit {
+export class WorkingWithObservablesComponent implements OnInit, AfterViewInit {
 
-  mouseXY = {
-    x: 0,
-    y: 0
-  };
+  movies: Array<any> = [];
 
-  constructor() { }
+  @ViewChild('btn') btnElm: ElementRef;
 
-  ngOnInit() {
-    let source$ = Observable.fromEvent(document, 'mousemove')
-      .pipe(
-        map(
-          (m: MouseEvent) => {
-            return {
-              x: m.clientX,
-              y: m.clientY
-            }
-          }
-        ),
-        filter(value => value.x < 500),
-        delay(300)
-      );
-
-    source$.subscribe(
-      value => {
-        this.mouseXY = value;
-        console.log(value)
-      }
-    )
+  constructor() {
   }
 
+  ngOnInit() {
+
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.btnElm.nativeElement);
+    let click$ = Observable.fromEvent(this.btnElm.nativeElement, 'click');
+
+    click$.pipe(
+      flatMap(e => this.load('/assets/movies.json'))
+    ).subscribe(
+      o => this.movies = o
+    );
+
+    // click$.subscribe(
+    //   value => {
+    //     this.load('/assets/movies.json').subscribe(value => {
+    //       this.movies = value;
+    //     });
+    //     console.log(value);
+    //   }
+    // );
+  }
+
+  private load(url: string): Observable<any> {
+
+    return Observable.create(observer => {
+      let xhr = new XMLHttpRequest();
+
+      xhr.addEventListener('load', () => {
+        observer.next(JSON.parse(xhr.responseText));
+        observer.complete();
+      });
+
+      xhr.open('GET', url);
+      xhr.send();
+
+    });
+  }
 }
