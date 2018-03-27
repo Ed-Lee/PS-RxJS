5b5efa3135fdc0f3068cae4e0e8f0567535f708d
Ed Lee <ronder.lee@gmail.com>
2018-03-27 11:16:40 +0800

    Implementing Retry Logic with retry and retryWhen retryWhen(給一個傳入Observable參數並傳回Observable的函數)，所以可以先寫一個簡單function, 裡面傳回一個 function來達成，這個範例改成每隔[delayT]秒試一次，不超[attempts]次

--- src/app/working-with-observables/working-with-observables.component.ts
+++ src/app/working-with-observables/working-with-observables.component.ts
@@ -62,20 +62,20 @@ export class WorkingWithObservablesComponent implements OnInit, AfterViewInit {
 
     }).pipe(
 
-      retryWhen(this.retryStrategy())
+      retryWhen(this.retryStrategy({attempts: 4, delayT: 1500}))
     );
   }
 
-  private retryStrategy() {
+  private retryStrategy({attempts=4, delayT=1000}) {
     //retryWhen的參數是一個傳入Observable並傳回Observable的函數
-    return function(errors: Observable<any>):Observable<any> { //該函數主要功能就是要每隔一秒試一次，不超4次
+    return function(errors: Observable<any>):Observable<any> { //該函數主要功能就是要每隔[delayT]秒試一次，不超[attempts]次
       return errors.pipe(
         scan((acc, value) => {
           console.log(acc, value);
           return acc + 1
         }, 0),
-        takeWhile((acc => acc < 4)),
-        delay(1000)
+        takeWhile((acc => acc < attempts)),
+        delay(delayT)
       );
     }
   }
