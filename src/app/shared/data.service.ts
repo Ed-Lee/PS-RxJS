54e7dedc90b67fa1e0d9900603975b19d9cde9dd
Ed Lee <ronder.lee@gmail.com>
2018-03-27 14:27:37 +0800

    Dealing with Errors and Exceptions 1. observer.error("msg") 與 throw new Error("err msg")都會使程式停下 2. subscript(指定error handler), 則會被執行該handler, 程式也會停止 3. throw new Error("err msg"), 則會被執行該handler，程式也會停止, 只是多console紅字

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs/Observable";
+import {delay, retryWhen, scan, takeWhile} from "rxjs/operators";
 
 @Injectable()
 export class DataService {
@@ -24,6 +25,48 @@ export class DataService {
         produceValue();
       }
     );
+  }
+
+  loadWithFetch(url: string) {
+    return Observable.defer(
+      () => Observable.fromPromise(fetch(url).then(r => r.json()))
+    )
+  }
+
+  load(url: string): Observable<any> {
+
+    return Observable.create(observer => {
+      let xhr = new XMLHttpRequest();
+
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          observer.next(JSON.parse(xhr.responseText));
+          observer.complete();
+        } else {
+          observer.error(xhr.statusText);
+        }
+      });
+
+      xhr.open('GET', url);
+      xhr.send();
+
+    }).pipe(
+      retryWhen(this.retryStrategy({attempts: 4, delayT: 1500}))
+    );
+  }
+
+  retryStrategy({attempts=4, delayT=1000}) {
+    //retryWhen的參數是一個傳入Observable並傳回Observable的函數
+    return function(errors: Observable<any>):Observable<any> { //該函數主要功能就是要每隔[delayT]秒試一次，不超[attempts]次
+      return errors.pipe(
+        scan((acc, value) => {
+          console.log(acc, value);
+          return acc + 1
+        }, 0),
+        takeWhile((acc => acc < attempts)),
+        delay(delayT)
+      );
+    }
   }
 
 }
--- src/app/working-with-observable-data/working-with-observable-data.component.ts
+++ src/app/working-with-observable-data/working-with-observable-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-working-with-observable-data',
@@ -10,6 +11,20 @@ export class WorkingWithObservableDataComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let source$ = Observable.create(observer => {
+      observer.next(1);
+      observer.next(2);
+      observer.error("Stop!");
+      //throw new Error(" trhow Error , Stop!");
+      observer.next(3);
+      observer.complete();
+    });
+
+    source$.subscribe(
+      value => console.log(`value: ${value}`),
+      err => console.log(`Error: ${err}`),
+      () => console.log('Complete')
+    );
   }
 
 }
